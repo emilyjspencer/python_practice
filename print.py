b6552ad9de3c677399dda085bc2bd1643641532e
lajoiedevivre89 <emilyspencer89@hotmail.co.uk>
2020-02-27 10:06:44 +0000

    Print

---
+++ print.py
@@ -0,0 +1,17 @@
+name = "emily"
+print(name.upper())
+
+#=> EMILY
+
+book = "The Midnight Library - Matt Haig"
+print (book.upper())
+
+#=> THE MIDNIGHT LIBRARY - MATT HAIG
+
+print(len(book))
+
+#=> 32
+
+print("I" + " " + "like" + " "  + "whitespace")
+
+=> I like whitespace
