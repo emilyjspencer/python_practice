dae5529959835fae90343227360a29722e368d0b
Emily Spencer <emilyspencer89@hotmail.co.uk>
2020-03-23 22:35:52 +0000

    Exponentation

---
+++ exponentiation.py
@@ -0,0 +1,6 @@
+def exponentiate(base, exponent):  
+  result = base ** exponent
+  print("%d to the power of %d is %d." % (base, exponent, result))
+
+exponentiate(2,3) 
+#=> 2 to the power of 3 is 8.
