a60fcb3bd1fbebee065af28f5451e1fdf0b21566
Emily Spencer <emilyspencer89@hotmail.co.uk>
2020-03-23 18:30:05 +0000

    datetime

---
+++ date.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+
+now = datetime.now()
+print(now)
+print(now.year)
+print(now.month)
+print(now.day)
+print(now.hour)
+print(now.minute)
+print(now.second)
+
+#=> 2020-03-23 18:29:14.669083
+#=> 2020
+#=> 3
+#=> 23
+#=> 18
+#=> 29
+#=> 14
