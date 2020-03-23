bae34afd83c160994efbba9d625dce2c9ed4a7bc
Emily Spencer <emilyspencer89@hotmail.co.uk>
2020-03-23 17:46:43 +0000

    Python syntax

---
+++ python_syntax.py
@@ -0,0 +1,8 @@
+skill_completed = "Python Syntax"
+exercises_completed = 13
+
+points_per_exercise = 2
+point_total = 100
+point_total += exercises_completed*points_per_exercise
+
+print("I got "+ str(point_total)+ " points!")
