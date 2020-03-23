e374599b822c1de0584ea6e7938ba11bf2725a7a
Emily Spencer <emilyspencer89@hotmail.co.uk>
2020-03-23 18:15:03 +0000

    Using input()

---
+++ using_input.py
@@ -0,0 +1,11 @@
+name = input("What is your name? ")
+quarantine = input("How are you finding quarantine life? ")
+holiday_destination = input("What is your favourite holiday destination? ")
+
+print("So, you are called %s, you are finding quarantine life to be %s, " \
+"and your favorite holiday destination is %s." % (name, quarantine, holiday_destination))
+
+#=> What is your name? Emily
+#=> How are you finding quarantine life? ok
+#=> What is your favourite holiday destination? Paris
+#=> So, you are called Emily, you are finding quarantine life to be ok, and your favorite holiday destination is Paris.
