0d82f2a087b2aecf62e2eac434c77f3be0b30245
Emily Spencer <emilyspencer89@hotmail.co.uk>
2020-03-23 18:11:38 +0000

    Formatting

---
+++ formatting.py
@@ -0,0 +1,14 @@
+name = "Meryl"
+string = "Hello, my name is "
+
+sentence = string + name 
+print(sentence)
+#=> "Hello, my name is Meryl"
+
+name = "Goldie"
+print("Hello %s" % (name))
+#=> "Hello Goldie"
+
+nationality = "American"
+print("I am %s" % (nationality))
+#=> "I am American"
