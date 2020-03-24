00c0bd909a5f5818073497c94a5a71373caefe58
Emily Spencer <emilyspencer89@hotmail.co.uk>
2020-03-24 14:01:01 +0000

    Dictionaries

---
+++ dictionaries.py
@@ -0,0 +1,10 @@
+
+disney_films = {}
+disney_films["Cinderella"] = 1950
+disney_films["The Little Mermaid"] = 1990
+disney_films["Bambi"] = 1942
+disney_films["The Lion Kings"] = 1994
+disney_films["Hercules"] = 1997
+
+
+print(disney_films)
