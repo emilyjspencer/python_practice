b37cc758622ae1a87201233de8f5b5eb8bdb1386
Emily Spencer <emilyspencer89@hotmail.co.uk>
2020-03-24 11:40:28 +0000

    Shopping trolley

---
+++ shopping_trolley.py
@@ -0,0 +1,14 @@
+
+
+shopping_trolley = []
+
+shopping_trolley.append("oranges")
+shopping_trolley.append("shampoo")
+shopping_trolley.append("cereal")
+
+list_length = len(shopping_trolley)
+
+
+print("There are %d items in the shopping_trolley." % (list_length))
+print("As per usual, there was no toilet paper, no paracetamol, no pasta, no rice, no disinfectant, no soap")
+print("I love panic buyers - I really do.....")
