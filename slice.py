86c05560018c7ed1528272a6a0241f0bf7ec1970
Emily Spencer <emilyspencer89@hotmail.co.uk>
2020-03-24 13:02:54 +0000

    Slice method

---
+++ slice.py
@@ -0,0 +1,20 @@
+#=> Use of slice()
+
+languages = "frenchgermandutchspanishitalianswedishpolishmandarin"
+
+
+french = languages[:6]
+
+
+german = languages[6:12]
+
+
+dutch = languages[12:17]
+
+print(french)
+print(german)
+print(dutch)
+
+#=> french
+#=> german
+#=> dutch
